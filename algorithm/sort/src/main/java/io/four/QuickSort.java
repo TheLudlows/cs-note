6464ad4ad0ee3b75533f75df93e1eef98f178704
four <ai-l@outlook.com>
2021-05-17 22:38:26 +0800

    add

--- algorithm/sort/src/main/java/io/four/QuickSort.java
+++ algorithm/sort/src/main/java/io/four/QuickSort.java
@@ -17,39 +17,20 @@ public class QuickSort {
             quickSort(arr, low, partition - 1);
             quickSort(arr, partition + 1, high);
         }
-
     }
 
     public static int partition(int[] arr, int low, int high) {
-        int index = low + 1;
         int pivot = low;
+        int j = low + 1; // 小于p的下标
         int p = arr[low];
-        for (int i = low; i <= high; i++) {
+        for (int i = j; i <= high; i++) {
             if (arr[i] < p) {
-                swap(arr, index++, i);  //满足条件交换位置，且index向右移动
+                swap(arr, j++, i);  //满足条件交换位置，且index向右移动
             }
         }
-        swap(arr, pivot, index - 1);
-        return index - 1;
+        swap(arr, pivot, j - 1);
+        return j - 1;
     }
-
-
-    public int par(Integer[] list, int l, int r) {
-        int p = list[l];    //数组的第一个作为中轴
-        while (l < r) {
-            while (l < r && list[r] >= p) {
-                r--;
-            }
-            list[l] = list[r];   //比中轴小的记录移到低端
-            while (l < r && list[l] <= p) {
-                l++;
-            }
-            list[r] = list[l];   //比中轴大的记录移到高端
-        }
-        list[l] = p;              //中轴记录到尾
-        return l;                   //返回中轴的位置
-    }
-
 
     public static void swap(int[] arr, int high, int low) {
         int temp = arr[low];
